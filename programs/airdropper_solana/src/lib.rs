31ce54294155004167fab303639a2b43f04471d2
utsavempiric20 <utsav@empiricinfotech.com>
2025-07-02 19:20:08 +0530

    feat: develop the claim and initialize instruction for airdrop

--- programs/airdropper_solana/src/lib.rs
+++ programs/airdropper_solana/src/lib.rs
@@ -1,16 +1,153 @@
-use anchor_lang::prelude::*;
+#![allow(unexpected_cfgs)]
 
-declare_id!("3skgrkCMK7ntA99KBJGaTB5DWHpyrT3QgzCkVyGcHGtb");
+use anchor_lang::prelude::*;
+use anchor_spl::{ associated_token::AssociatedToken, token::{ Mint, Token, TokenAccount } };
+use std::mem::size_of;
+
+declare_id!("5zp47zmoPwVa55PXtP5kr7URsNRMUqnhiPLLnyo5M9AQ");
+pub const MAX_LEAVES: usize = 10;
+pub const BITMAP_BYTES: usize = (MAX_LEAVES + 7) / 8;
 
 #[program]
 pub mod airdropper_solana {
+    use anchor_lang::solana_program::keccak::hashv;
+    use anchor_spl::token::{ transfer, Transfer };
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(
+        ctx: Context<InitializeDistributor>,
+        merkle_root: [u8; 32],
+        total_supply: u128
+    ) -> Result<()> {
+        let distributor = &mut ctx.accounts.distributor;
+        let (_key, bump) = Pubkey::find_program_address(
+            &[b"distributor", distributor.key().as_ref()],
+            ctx.program_id
+        );
+        distributor.merkle_root = merkle_root;
+        distributor.vault = ctx.accounts.distributor_token_account.key();
+        distributor.token_mint = ctx.accounts.token_mint.key();
+        distributor.total_supply = total_supply;
+        distributor.claimed_bitmap = [0u8; BITMAP_BYTES];
+        distributor.bump = bump;
+        distributor.authority = ctx.accounts.distributor_authority.key();
+        Ok(())
+    }
+
+    pub fn claim(ctx: Context<Claim>, index: u32, amount: u64, proof: Vec<[u8; 32]>) -> Result<()> {
+        let distributor = &mut ctx.accounts.distributor;
+
+        let leaf = hashv(
+            &[
+                &index.to_le_bytes(),
+                ctx.accounts.user_token_account.owner.as_ref(),
+                &amount.to_le_bytes(),
+            ]
+        ).0;
+
+        let mut computed = leaf;
+        for sibling in proof.iter() {
+            let (l, r) = if computed <= *sibling {
+                (computed, *sibling)
+            } else {
+                (*sibling, computed)
+            };
+            computed = hashv(&[&l, &r]).0;
+        }
+        require!(computed == distributor.merkle_root, AirdropError::InvalidMerkleRoot);
+
+        let byte_index = (index / 8) as usize;
+        let bitmask = 1 << index % 8;
+        distributor.claimed_bitmap[byte_index] |= bitmask;
+
+        require!(
+            (distributor.claimed_bitmap[byte_index] & bitmask) == 0,
+            AirdropError::AlreadyClaimed
+        );
+
+        let binding = distributor.key();
+        let vault_seeds = &[b"distributor", binding.as_ref(), &[distributor.bump]];
+        let seeds = &[&vault_seeds[..]];
+        let cpi_accounts = Transfer {
+            from: ctx.accounts.distributor_token_account.to_account_info(),
+            to: ctx.accounts.user_token_account.to_account_info(),
+            authority: ctx.accounts.distributor_authority.to_account_info(),
+        };
+
+        transfer(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                cpi_accounts,
+                seeds
+            ),
+            amount
+        )?;
+
         Ok(())
     }
 }
 
+#[account]
+pub struct Distributor {
+    pub merkle_root: [u8; 32],
+    pub vault: Pubkey,
+    pub bump: u8,
+    pub token_mint: Pubkey,
+    pub total_supply: u128,
+    pub claimed_bitmap: [u8; BITMAP_BYTES],
+    pub authority: Pubkey,
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(merkle_root:[u8;32])]
+pub struct InitializeDistributor<'info> {
+    #[account(init, space = 8 + size_of::<Distributor>() + BITMAP_BYTES, payer = payer)]
+    pub distributor: Account<'info, Distributor>,
+
+    /// CHECK
+    #[account(seeds = [b"distributor", distributor.key().as_ref()], bump)]
+    pub distributor_authority: UncheckedAccount<'info>,
+
+    #[account(mut)]
+    pub distributor_token_account: Account<'info, TokenAccount>,
+
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    pub token_mint: Account<'info, Mint>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
+#[derive(Accounts)]
+pub struct Claim<'info> {
+    #[account(mut, has_one = token_mint)]
+    pub distributor: Account<'info, Distributor>,
+
+    /// CHECK
+    #[account(seeds = [b"distributor", distributor.key().as_ref()], bump)]
+    pub distributor_authority: UncheckedAccount<'info>,
+
+    #[account(mut)]
+    pub distributor_token_account: Account<'info, TokenAccount>,
+
+    #[account(mut)]
+    pub user_token_account: Account<'info, TokenAccount>,
+
+    pub token_mint: Account<'info, Mint>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
+#[error_code]
+pub enum AirdropError {
+    #[msg("Invalid Merkle Root")]
+    InvalidMerkleRoot,
+    #[msg("Already Claimed")]
+    AlreadyClaimed,
+}
--- tests/airdropper_solana.ts
+++ tests/airdropper_solana.ts
@@ -1,16 +1,168 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AirdropperSolana } from "../target/types/airdropper_solana";
+import { MerkleTree } from "merkletreejs";
+import keccak256 from "keccak256";
+import fs from "fs";
+import { Keypair, PublicKey, SYSVAR_RENT_PUBKEY } from "@solana/web3.js";
+import {
+  createAccount,
+  createAssociatedTokenAccount,
+  createMint,
+  mintTo,
+  TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
+import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet";
+import { SYSTEM_PROGRAM_ID } from "@coral-xyz/anchor/dist/cjs/native/system";
+import { ASSOCIATED_PROGRAM_ID } from "@coral-xyz/anchor/dist/cjs/utils/token";
 
 describe("airdropper_solana", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.AirdropperSolana as Program<AirdropperSolana>;
+  const program = anchor.workspace
+    .AirdropperSolana as Program<AirdropperSolana>;
+  const payer = (provider.wallet as NodeWallet).payer;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const airdropList: Array<{ address: string; amount: number }> = [
+    { address: "5Gz…abc", amount: 100 },
+    { address: "F7x…xyz", amount: 250 },
+  ];
+
+  const leaves = airdropList.map(({ address, amount }, i) =>
+    keccak256(
+      Buffer.concat([
+        Buffer.from(i.toString(16).padStart(8, "0"), "hex"),
+        Buffer.from(address, "hex"),
+        Buffer.from(amount.toString(16).padStart(16, "0"), "hex"),
+      ])
+    )
+  );
+  console.log("leaves : ", leaves);
+
+  const tree = new MerkleTree(leaves, keccak256, { sortPairs: true });
+  console.log("tree : ", tree);
+
+  const root = tree.getRoot().toString("hex");
+  console.log("root : ", root);
+  const merkleRoot = Buffer.from(root, "hex");
+
+  fs.writeFileSync("./merkle-root.txt", root, "utf8");
+
+  for (let i = 0; i < leaves.length; i++) {
+    const proof = tree.getProof(leaves[i]).map((x) => x.data.toString("hex"));
+    console.log("proof1 : ", proof);
+
+    fs.writeFileSync(`./proof-${i}.json`, JSON.stringify(proof));
+  }
+
+  let distributor = Keypair.generate();
+  let distributor_pubkey = distributor.publicKey;
+  let tokenMint: PublicKey;
+  let vault_authority: PublicKey;
+  let vaultBump: any;
+  let vaultTokenAccount: PublicKey;
+  let userAta: PublicKey;
+
+  before("Derive PDA and Mint", async () => {
+    tokenMint = await createMint(
+      provider.connection,
+      payer,
+      payer.publicKey,
+      null,
+      9
+    );
+    console.log("tokenMint : ", tokenMint.toBase58());
+
+    userAta = await createAccount(
+      provider.connection,
+      payer,
+      tokenMint,
+      provider.publicKey
+    );
+    console.log("userAta : ", userAta.toBase58());
+
+    [vault_authority, vaultBump] = await PublicKey.findProgramAddressSync(
+      [Buffer.from("distributor"), distributor_pubkey.toBuffer()],
+      program.programId
+    );
+    console.log("vault_authority : ", vault_authority.toBase58());
+
+    vaultTokenAccount = await anchor.utils.token.associatedAddress({
+      mint: tokenMint,
+      owner: vault_authority,
+    });
+    console.log("vaultTokenAccount : ", vaultTokenAccount.toBase58());
+
+    await createAssociatedTokenAccount(
+      provider.connection,
+      payer,
+      tokenMint,
+      vault_authority,
+      null,
+      TOKEN_PROGRAM_ID,
+      ASSOCIATED_PROGRAM_ID,
+      true
+    );
+    console.log("created");
+    await mintTo(
+      provider.connection,
+      payer,
+      tokenMint,
+      vaultTokenAccount,
+      payer.publicKey,
+      1000000000000
+    );
+    console.log("minted");
+  });
+
+  it("Initialize the airdrop!", async () => {
+    const amount = new anchor.BN(1000000000000);
+    await program.methods
+      .initialize(Array.from(merkleRoot), amount)
+      .accountsStrict({
+        payer: payer.publicKey,
+        distributor: distributor.publicKey,
+        distributorAuthority: vault_authority,
+        distributorTokenAccount: vaultTokenAccount,
+        tokenMint,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        systemProgram: SYSTEM_PROGRAM_ID,
+        associatedTokenProgram: ASSOCIATED_PROGRAM_ID,
+        rent: SYSVAR_RENT_PUBKEY,
+      })
+      .signers([distributor])
+      .rpc();
+  });
+
+  it("Transfer AirdropTokens", async () => {
+    for (let i = 0; i < airdropList.length; i++) {
+      const { address, amount } = airdropList[i];
+
+      const leaf = leaves[i];
+      console.log("tree.getProof(leaf) : ", tree.getProof(leaf));
+
+      const proof = tree.getProof(leaf).map((x) => Array.from(x.data));
+
+      console.log(`⏳ Airdropping to #${i} → ${address} (${amount})`);
+      console.log("proof :", proof);
+
+      await program.methods
+        .claim(i, new anchor.BN(amount), proof)
+        .accountsStrict({
+          distributor: distributor.publicKey,
+          distributorTokenAccount: vaultTokenAccount,
+          userTokenAccount: userAta,
+          associatedTokenProgram: ASSOCIATED_PROGRAM_ID,
+          distributorAuthority: vault_authority,
+          rent: SYSVAR_RENT_PUBKEY,
+          systemProgram: SYSTEM_PROGRAM_ID,
+          tokenMint,
+          tokenProgram: TOKEN_PROGRAM_ID,
+        })
+        .rpc();
+
+      console.log(`✅ Done #${i}`);
+    }
   });
 });
